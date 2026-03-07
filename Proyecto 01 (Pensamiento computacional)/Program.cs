2bff79b04654f3b45834ebcbfafdf33980438575
verno <verno@ASUS>
2026-03-07 10:30:08 -0600

    switch solo titulos implementado junto con do while

--- Proyecto 01 (Pensamiento computacional)/Program.cs
+++ Proyecto 01 (Pensamiento computacional)/Program.cs
@@ -3,7 +3,28 @@ int opcion_menu = 0;
 do
 {
     switch (opcion_menu)
-        {
-      
-    }
+     {
+        Console.WriteLine("1. Evaluar nuevo contenido \r 2. Mostrar reglas del sistema \r 3. Mostrar  estadísticas de la sesión \r4. Reiniciar  estadísticas \r 5.Salir");
+        opcion_menu = int.Parse(Console.ReadLine());
+        case 1:
+            Console.WriteLine("Evaluar nuevo contenido");
+            break;
+
+        case 2:
+            Console.WriteLine("Mostrar reglas del contenido");
+            break;
+        case 3:
+            Console.WriteLine("Mostrar estadísticas de la sesión ");
+            break;
+        case 4:
+            Console.WriteLine("Reiniciar estadísticas ");
+            break;
+        case 5:
+            Console.WriteLine("Salir");
+            break;
+        default:
+            Console.WriteLine("Opción no válida, por favor ingrese una opción del 1 al 5.");
+            break;
+     }
 }
+while (opcion_menu != 5);
