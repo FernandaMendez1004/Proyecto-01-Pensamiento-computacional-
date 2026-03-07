0bf020e87789107b399bee9229baf187b0af452a
verno <verno@ASUS>
2026-03-07 10:56:08 -0600

    menu arreglado

--- Proyecto 01 (Pensamiento computacional)/Program.cs
+++ Proyecto 01 (Pensamiento computacional)/Program.cs
@@ -2,29 +2,42 @@
 int opcion_menu = 0;
 do
 {
-    switch (opcion_menu)
-     {
-        Console.WriteLine("1. Evaluar nuevo contenido \r 2. Mostrar reglas del sistema \r 3. Mostrar  estadísticas de la sesión \r4. Reiniciar  estadísticas \r 5.Salir");
-        opcion_menu = int.Parse(Console.ReadLine());
-        case 1:
-            Console.WriteLine("Evaluar nuevo contenido");
-            break;
+    Console.WriteLine("1. Evaluar nuevo contenido");
+    Console.WriteLine("2. Mostrar reglas del sistema");
+    Console.WriteLine("3. Mostrar estadísticas de la sesión");
+    Console.WriteLine("4. Reiniciar estadísticas");
+    Console.WriteLine("5. Salir");
+    Console.WriteLine("Elija una opción:");
+    opcion_menu = int.Parse(Console.ReadLine());
 
-        case 2:
-            Console.WriteLine("Mostrar reglas del contenido");
-            break;
-        case 3:
-            Console.WriteLine("Mostrar estadísticas de la sesión ");
-            break;
-        case 4:
-            Console.WriteLine("Reiniciar estadísticas ");
-            break;
-        case 5:
-            Console.WriteLine("Salir");
-            break;
-        default:
-            Console.WriteLine("Opción no válida, por favor ingrese una opción del 1 al 5.");
-            break;
-     }
+
+    switch (opcion_menu)
+    {
+        case 1:
+        Console.WriteLine("Evaluar nuevo contenido");
+        break;
+
+    case 2:
+        Console.WriteLine("Mostrar reglas del contenido");
+        break;
+
+    case 3:
+        Console.WriteLine("Mostrar estadísticas de la sesión ");
+        break;
+
+    case 4:
+        Console.WriteLine("Reiniciar estadísticas ");
+        break;
+
+    case 5:
+        Console.WriteLine("Salir");
+        break;
+
+    default:
+        Console.WriteLine("Opción no válida, por favor ingrese una opción del 1 al 5.");
+        break;
+    }
+    Console.WriteLine("Presione Enter para continuar...");
+    Console.ReadLine();
 }
 while (opcion_menu != 5);
